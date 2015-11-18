c14675ee7cbd373fd4bc053f5eb6a57aa5b0d301
tomhagan <thomasphagan@gmail.com>
2015-11-17 23:57:08 -0500

    First commit. All files.

---
+++ CustomVision/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomVision.MvcApplication" Language="C#" %>
---
+++ CustomVision/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomVision.Startup))]
+namespace CustomVision
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
