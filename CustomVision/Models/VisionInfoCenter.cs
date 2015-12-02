5e3cbde7307d925d16ab9485a2bb4daf6e59aac4
tomhagan <thomasphagan@gmail.com>
2015-12-01 23:30:03 -0500

    Added automatic database migration via Tools > NuGet Package Manager console.

---
+++ CustomVision/Models/VisionInfoCenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CustomVision.Models
+{
+    //public class VisionInfoCenter
+    //{
+    //    /* Class VisionInfoCenter notes
+    //     * Going to try to re-create the "Info Centers" in Vision
+    //     * as categories. Then have reports rendered in each category.
+    //     */
+
+    //    public int ID { get; set; }
+    //    public string Title { get; set; }
+    //    public DateTime ReleaseDate { get; set; }
+    //    public string Genre { get; set; }
+    //    public decimal Price { get; set; }
+
+    //}
+
+    //public class VisionInfoCenterDbContext : DbContext
+    //{
+    //    public DbSet<VisionInfoCenter> VisionInfoCenters { get; set; }
+    //}
+}
